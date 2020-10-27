faf7acd5620fe84c174ed488b3dfc352b723acec
CastilloObregon <manuelecastilloo@gmail.com>
2020-10-27 04:46:08 -0600

    algunos cambios

--- clasificador.py
+++ clasificador.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 
+import librosa
+
 import numpy as np
 import IPython.display as display
 
 
 #Cargar los TFRecords
 
-filenames = [filename]
+filenames = ["TFRecords/nsynth-train.tfrecord"]
 raw_dataset = tf.data.TFRecordDataset(filenames)
 raw_dataset
